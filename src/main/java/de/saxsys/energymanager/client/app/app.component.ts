6a7c339dfd3712634838d1dc730cc1c5da6fd91c
Stefan Illgen <stefan.illgen@saxsys.de>
2016-07-18 15:39:59 +0200

    bindings explored

--- src/main/java/de/saxsys/energymanager/client/app/app.component.ts
+++ src/main/java/de/saxsys/energymanager/client/app/app.component.ts
@@ -1,7 +1,39 @@
 import { Component } from '@angular/core';
 
+// class definition
+export class SolarPanel {
+  name: string;
+}
+export class MonitoringEntry {
+  generatorPower: number;
+}
+export class MonitoringData {
+  //solarPanel: SolarPanel;
+  //entries: Array<MonitoringEntry>;
+  days: number;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>All Solar Panels</h2>
+    <h2>Monitoring Data for {{solarPanel.name}}</h2>
+    <div>
+      <label>Days: </label>
+      <input [(ngModel)]="monitoringData.days" placeholder="days">
+    </div>
+    <div>
+      <p>Chart with monioring entries for {{monitoringData.days}} days.</p>
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Distributed Energy Management'
+  solarPanel: SolarPanel = {
+    name: 'A Panel'
+  }
+  monitoringData: MonitoringData = {
+    days: 3
+  }
+}
