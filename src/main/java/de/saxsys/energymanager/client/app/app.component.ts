0a9fd212dcfccbdc5f38d5fae8d69c6bfd79a6ca
Stefan Illgen <stefan.illgen@saxsys.de>
2016-07-19 12:37:22 +0200

    clean up

--- src/main/java/de/saxsys/energymanager/client/app/app.component.ts
+++ src/main/java/de/saxsys/energymanager/client/app/app.component.ts
@@ -54,10 +54,6 @@ const ALL_MONITORING_DATA: MonitoringData[] = [
     </div>
     <button>
     <div *ngIf="selectedMonitoringData">
-      <h3>Monitoring Data for {{selectedMonitoringData.solarPanel.name}}</h3>
-      <div>
-        <p>Chart with monioring entries for {{monitoringDataInput.days}} days.</p>
-      </div>
       <base-chart class="chart"
           [datasets]="lineChartData"
           [labels]="lineChartLabels"
@@ -132,37 +128,22 @@ export class AppComponent {
     this.selectedMonitoringData = monitoringData;
     this.lineChartData = [
       {
-        data: this.selectedMonitoringData.entries,//[65, 59, 80, 81, 56, 55, 40],
+        data: this.selectedMonitoringData.entries,
         label: this.selectedMonitoringData.solarPanel.name
       }
     ];
+    this.lineChartLabels = this.selectedMonitoringData.entries.map((entry, index) => index)
   };
 
   // line chart
   public lineChartData:Array<any>;
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any>;
   public lineChartOptions:any = {
     animation: false,
     responsive: true
   };
   public lineChartColours:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
+    {
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
