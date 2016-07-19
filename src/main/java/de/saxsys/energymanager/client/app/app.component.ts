fac561a00c8cab0a764ab36219ff974faef6ec8a
Stefan Illgen <stefan.illgen@saxsys.de>
2016-07-19 10:55:16 +0200

    show mocked data in chart

--- src/main/java/de/saxsys/energymanager/client/app/app.component.ts
+++ src/main/java/de/saxsys/energymanager/client/app/app.component.ts
@@ -58,14 +58,15 @@ const ALL_MONITORING_DATA: MonitoringData[] = [
       <div>
         <p>Chart with monioring entries for {{monitoringDataInput.days}} days.</p>
       </div>
-    </div>
-    <base-chart class="chart"
-                [data]="doughnutChartData"
-                [labels]="doughnutChartLabels"
-                [chartType]="doughnutChartType"
-                (chartHover)="chartHovered($event)"
-                (chartClick)="chartClicked($event)">
+      <base-chart class="chart"
+          [datasets]="lineChartData"
+          [labels]="lineChartLabels"
+          [options]="lineChartOptions"
+          [colors]="lineChartColours"
+          [legend]="lineChartLegend"
+          [chartType]="lineChartType">
       </base-chart>
+    </div>
     `,
     styles: [`
     .selected {
@@ -129,19 +130,47 @@ export class AppComponent {
 
   onSelect(monitoringData: MonitoringData) {
     this.selectedMonitoringData = monitoringData;
+    this.lineChartData = [
+      {
+        data: this.selectedMonitoringData.entries,//[65, 59, 80, 81, 56, 55, 40],
+        label: this.selectedMonitoringData.solarPanel.name
+      }
+    ];
   };
 
-
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
-
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
-
+  // line chart
+  public lineChartData:Array<any>;
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions:any = {
+    animation: false,
+    responsive: true
+  };
+  public lineChartColours:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend:boolean = true;
+  public lineChartType:string = 'line';
 }
