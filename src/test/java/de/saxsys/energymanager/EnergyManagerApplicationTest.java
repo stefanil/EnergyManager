5ed65224074f6bcc48fa6e1e43650a30ae61dcaa
Stefan Illgen <stefan.illgen@saxsys.de>
2016-08-03 11:16:22 +0200

    stiel

--- src/test/java/de/saxsys/energymanager/EnergyManagerApplicationTest.java
+++ src/test/java/de/saxsys/energymanager/EnergyManagerApplicationTest.java
@@ -1,5 +1,6 @@
 package de.saxsys.energymanager;
 
+import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -8,10 +9,14 @@ import static org.mockito.Mockito.when;
 import de.saxsys.energymanager.configuration.EnergyManagerConfiguration;
 import de.saxsys.energymanager.resources.SolarPanelsResource;
 
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.servlet.FilterRegistration.Dynamic;
+
 import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.jetty.setup.ServletEnvironment;
 import io.dropwizard.setup.Environment;
 
 /**
@@ -23,10 +28,15 @@ public class EnergyManagerApplicationTest {
   private final JerseyEnvironment jersey = mock(JerseyEnvironment.class);
   private final EnergyManagerApplication application = new EnergyManagerApplication();
   private final EnergyManagerConfiguration energyManagerConfiguration = new EnergyManagerConfiguration();
+  private final ServletEnvironment servletEnvironment = mock(ServletEnvironment.class);
+  private final Dynamic crossOriginFilter = mock(Dynamic.class);
 
   @Before
   public void setup() throws Exception {
     when(environment.jersey()).thenReturn(jersey);
+    when(environment.servlets()).thenReturn(servletEnvironment);
+    when(servletEnvironment.addFilter(eq("CORS"), eq(CrossOriginFilter.class)))
+        .thenReturn(crossOriginFilter);
   }
 
   @Test
