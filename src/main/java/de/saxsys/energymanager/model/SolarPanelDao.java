75926fe340083c426b05abb97b0b4c88b02f748c
Stefan Illgen <stefan.illgen@saxsys.de>
2016-08-04 09:42:23 +0200

    zum

--- src/main/java/de/saxsys/energymanager/model/SolarPanelDao.java
+++ src/main/java/de/saxsys/energymanager/model/SolarPanelDao.java
@@ -2,13 +2,8 @@ package de.saxsys.energymanager.model;
 
 import de.saxsys.energymanager.api.SolarPanel;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SolarPanelDao {
-
-  private static final Logger LOG = LoggerFactory.getLogger(SolarPanelDao.class);
-
+  
   /* **************** dirty singleton **************** */
 
   private static SolarPanelDao instance;
